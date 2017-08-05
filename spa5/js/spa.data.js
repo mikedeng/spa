e454d7b2fe0aafd5c99170ca66e95cfedbf6ca5f
dengming <dengming@uniqueway.com>
2017-08-05 21:45:18 +0800

    ch 5

---
+++ spa5/js/spa.data.js
@@ -0,0 +1,15 @@
+/*
+ * spa.data.js
+ * Data module
+*
+
+/* jslint browser: true, continue: true,
+ devel: true, indent: 2, maxerr: 50,
+ newcap: true,nomen: true, plusplus: true,
+ regexp: true, sloppy: true, vars: false,
+ white: true
+*/
+
+spa.data = (function () {
+	return {};
+});
